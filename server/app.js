5849626ff71afae9ee0e13644b8cb28bc24e54d5
Arnab Mandal <arnabmandal90007@gmail.com>
2023-05-12 01:11:18 +0530

    backend initial commit

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: [process.env.FRONTEND_UTL],
+        origin: [process.env.FRONTEND_URL],
         methods: ["GET","POST","DELETE","PUT"],
         credentials: true,
     })
